6c1d729ab770a7589888534c9b4db516e67cf977
KITTY-0809ZWQ\pitro <pitroda.darshana@outlook.com>
2020-08-07 10:32:28 +0930

    Add project files.

---
+++ 00Data/EFModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManageAdminWebApp.Movie
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime Releasedate { get; set; }
+
+        public string Director { get; set; }
+
+        public string EmailAddress { get; set;}
+
+        public CharEnumerator Language { get; set; }
+
+        public string Category { get; set; }
+
+
+
+
+    }
+}
